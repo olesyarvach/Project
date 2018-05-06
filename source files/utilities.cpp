8bd81a84c21fbe78b4bc7c45c36bad2f978e377b
olesyarvach <33004075+olesyarvach@users.noreply.github.com>
2018-05-06 15:48:23 +0300

    Create utilities.cpp

---
+++ source files/utilities.cpp
@@ -0,0 +1,12 @@
+#include "utilities.h"
+#include <cctype>
+#include <algorithm>
+
+void trim(std::string & s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+		return !std::isspace(ch);
+	}));
+	s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+		return !std::isspace(ch);
+	}).base(), s.end());
+}
